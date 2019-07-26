b116d5529c423104ff04bdfaf116b0bac2f6014c
Jeffrey <jeromebro@msn.com>
2019-07-25 21:11:15 -0500

    Add files via upload

---
+++ DLL version 2/DNode.java
@@ -0,0 +1,15 @@
+
+public class DNode {
+	static int noOfLinkedList=0;
+	int data;
+	
+	DNode previousNode;
+	DNode nextMode;
+	public Object nextNode;
+	
+	DNode (int data){
+		this.data = data;
+		noOfLinkedList++;
+	}
+
+}
